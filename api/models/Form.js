9db044fb290ae0ea0cf3ee4a615fbf278c18f711
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-12-02 12:55:58 +0530

    form model update

--- api/models/Form.js
+++ api/models/Form.js
@@ -10,14 +10,45 @@ const formSchema = new mongoose.Schema({
   DateofJoin: { type: String, required: true },
   CDSDetails: { type: String, required: true },
 
+  // New fields added
+  academicQualifications: [
+    {
+      qualification: { type: String, required: true },
+      universityInstitute: { type: String, required: true },
+      status: { type: String, required: true },
+    },
+  ],
+
+  previousEmploymentDetails: [
+    {
+      employer: { type: String, required: true },
+    },
+  ],
+
+  civilStatus: { type: String, required: true },
+
+  spouseDetails: [
+    {
+      name: { type: String, required: true },
+      ID: { type: String, required: true },
+      placeOfWork: { type: String, required: true },
+      positionHeld: { type: String, required: true },
+    },
+  ],
+
+  emergencyContact: {
+    name: { type: String, required: true },
+    relationship: { type: String, required: true },
+    contactNo: { type: String, required: true },
+  },
+
   status: { type: String, default: "Pending" },
   Approved: { type: String, default: "not" },
-  document1:{
-    type:String,
-    default:"none"
-},
-// Pending, Completed
-
+  document1: {
+    type: String,
+    default: "none",
+  },
 });
-const Form = mongoose.model('Form',formSchema);
+
+const Form = mongoose.model("Form", formSchema);
 export default Form;
