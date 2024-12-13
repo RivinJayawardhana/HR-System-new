06b3e4ef048db9555658e88441ae8f3de685201c
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-12-13 10:22:29 +0530

    login image

--- Frontend/src/Pages/SignIn.jsx
+++ Frontend/src/Pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
-import { Link , useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signInStart, signInSuccess, singInFailure } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import OAuthenticate from "../Components/OAuthenticate";
@@ -53,57 +53,63 @@ export default function SignIn() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center" style={{
-            backgroundImage: `url('')`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-        }}>
-            <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-center text-2xl font-cinzel font-semibold">Sign In</p>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-5">
-                    <div>
-                        <Label value="Your email" />
-                        <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange} />
-                    </div>
-                    <div>
-                        <Label value="Your password" />
-                        <div className="relative">
-                            <TextInput type={showPassword ? "text" : "password"} placeholder="Password" id="password" onChange={handleChange} />
-                            <button type="button" className="absolute top-2 right-3 focus:outline-none" onClick={togglePasswordVisibility}>
-                                {showPassword ? (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.5c5.185 0 9.448 4.014 9.95 9.048a.944.944 0 0 1 0 .904C21.448 16.486 17.185 20.5 12 20.5S2.552 16.486 2.05 13.452a.944.944 0 0 1 0-.904C2.552 8.514 6.815 4.5 12 4.5zM12 6a9 9 0 0 0-8.72 6.752.944.944 0 0 1 0 .496A9 9 0 0 0 12 18a9 9 0 0 0 8.72-4.752.944.944 0 0 1 0-.496A9 9 0 0 0 12 6z" />
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 12.75a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5z" />
-                                    </svg>
-                                ) : (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 15a7 7 0 01-7-7M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                )}
-                            </button>
+        <div className="min-h-screen flex">
+            {/* Left Side with Image */}
+            <div
+                className="hidden lg:flex w-1/2 bg-cover bg-center"
+                style={{
+                    backgroundImage: "../assets/",
+                }}
+             
+            ></div>
+
+            {/* Right Side with Login Form */}
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-6">
+                <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
+                    <p className="text-center text-2xl font-cinzel font-semibold">Sign In</p>
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-5">
+                        <div>
+                            <Label value="Your email" />
+                            <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange} />
                         </div>
+                        <div>
+                            <Label value="Your password" />
+                            <div className="relative">
+                                <TextInput type={showPassword ? "text" : "password"} placeholder="Password" id="password" onChange={handleChange} />
+                                <button type="button" className="absolute top-2 right-3 focus:outline-none" onClick={togglePasswordVisibility}>
+                                    {showPassword ? (
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.5c5.185 0 9.448 4.014 9.95 9.048a.944.944 0 0 1 0 .904C21.448 16.486 17.185 20.5 12 20.5S2.552 16.486 2.05 13.452a.944.944 0 0 1 0-.904C2.552 8.514 6.815 4.5 12 4.5zM12 6a9 9 0 0 0-8.72 6.752.944.944 0 0 1 0 .496A9 9 0 0 0 12 18a9 9 0 0 0 8.72-4.752.944.944 0 0 1 0-.496A9 9 0 0 0 12 6z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 12.75a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5z" />
+                                        </svg>
+                                    ) : (
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 15a7 7 0 01-7-7M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+                        <Button className="bg-slate-500" gradientDuoTone="purpleToBlue" type="submit">
+                            Sign In
+                        </Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Forgot Password?</span>
+                        <Link to="/forgetPassword" className="text-blue-500">Click Here</Link>
                     </div>
-                    <Button className="bg-slate-500"  gradientDuoTone='purpleToBlue' type="submit">
-                      Sign In
-                    </Button>
-                    
-                </form>
-                <div className="flex gap-2 text-sm mt-5 ">
-                    <span>Forgot Password?</span>
-                    <Link to='/forgetPassword' className="text-blue-500">Click Here</Link>
-                </div>
-                <div className="flex gap-2 text-sm mt-5 ">
-                    <span>Don't have an account?</span>
-                    <Link to='/sign-up' className="text-blue-500">Sign Up</Link>
-                </div>
-                <div className="text-red-600">
-                    {error && (
-                        <Alert className="mt-5" color='failure'>
-                            {error}
-                        </Alert>
-                    )}
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Don't have an account?</span>
+                        <Link to="/sign-up" className="text-blue-500">Sign Up</Link>
+                    </div>
+                    <div className="text-red-600">
+                        {error && (
+                            <Alert className="mt-5" color="failure">
+                                {error}
+                            </Alert>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
